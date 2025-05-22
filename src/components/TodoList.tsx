7492385a30721eb928d6cecb514c13862f55b21e
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-22 16:54:58 +0200

    Initial commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,21 @@
+import { Todo } from "@/types";
+
+export default async function TodoList() {
+  const url = `${process.env.API_URL}/api/v1/todos`;
+  console.log("url", url);
+  const res = await fetch(url, {});
+  console.log("res?", res);
+  if (!res.ok) {
+    throw new Error("Kon todos niet laden");
+  }
+  const todos: Todo[] = await res.json();
+  return (
+    <div>
+      {todos.map((todo) => (
+        <p key={todo.id}>
+          {todo.title} {todo.isCompleted ? "✅" : "❌"}
+        </p>
+      ))}
+    </div>
+  );
+}
