6894485158f0515e89a5c4cd213c9f100a94d630
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-23 11:23:46 +0200

    simpele component check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 import "./page.css";
 import TodoList from "@/components/TodoList";
-import Loading from "./Loading";
-import { Suspense } from "react";
+// import Loading from "./Loading";
+// import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return (
-    // <div className="container">
-    //   <p>Test</p>
-    // </div>
-    <Suspense fallback={<Loading />}>
-      <TodoList />
-    </Suspense>
-  );
+  return <TodoList />;
 }
